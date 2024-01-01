1a6be00d6a7909df09000f34c366ddcf0ef1586d
BearValley24 <150714532+BearValley24@users.noreply.github.com>
2024-01-02 05:18:10 +0900

    Add kadai_005
    課題005を追加

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+let bodyWeight;
+let bodyHeight;
+
+bodyWeight = 68;
+bodyHeight = 1.7;
+
+const bodyHeightDual = bodyHeight * bodyHeight;
+const BMI = bodyWeight / bodyHeightDual
+
+console.log (BMI);
